f57a7deeca404b05203d78cdb783db75ca9ab828
Mateusz Łuczak <mati.maxx@o2.pl>
2023-03-31 09:46:42 +0200

    lab5

---
+++ lab5/src/main/java/org/tau/FakeMealDB.java
@@ -0,0 +1,35 @@
+package org.tau;
+
+public class FakeMealDB {
+    public String searchMeal(String url) {
+        return "";
+    }
+
+    public String searchMealDetailsById(String url) {
+        return "";
+    }
+
+    public String getAllCategories(String url) {
+        return "";
+    }
+
+    public String getAllAreas(String url) {
+        return "";
+    }
+
+    public String getAllIngredients(String url) {
+        return "";
+    }
+
+    public String filterByMainIngredient(String url) {
+        return "";
+    }
+
+    public String filterByCategory(String url) {
+        return "";
+    }
+
+    public String filterByArea(String url) {
+        return "";
+    }
+}
